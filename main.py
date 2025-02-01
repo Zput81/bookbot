4660caf987dec817b0256a7f7aef694a89683066
Zput81 <zputnam1369@gmail.com>
2025-01-31 20:42:26 -0500

    Word Count

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        words = file_contents.split()
+        count = 0
+        for i in words:
+            if i != None:
+                count += 1
+        print(count)
+
+main()
